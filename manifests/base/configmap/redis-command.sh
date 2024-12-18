ec8142f6080ea45357e667efae6ee55fbc2572d9
Julien Boulanger <julien.boulanger@claranet.com>
2024-12-18 13:48:26 +0100

    chore: update

--- manifests/base/configmap/redis-command.sh
+++ manifests/base/configmap/redis-command.sh
@@ -6,7 +6,6 @@ if ! command -v redis-cli &>/dev/null; then
 	exit 1
 fi
 
-TTL=$(redis-cli -n "${db_num}" -h kv-memory-redis-ha-announce-0.redis ttl "$LINE")
 export DEBIAN_FRONTEND=noninteractive
 apt update -qq && apt install -yqq curl
 DBS=$(/usr/local/bin/redis-cli -h kv-memory-redis-ha-announce-0.redis info keyspace | awk -F":" '{print $1}' | tail -n +2 | awk '{print substr($1,3); }')
@@ -30,6 +29,7 @@ for db_num in ${DBS}; do
 done
 
 CHANNEL="${CHANNEL_CI-@julien.boulanger}"
+SLACK_MSG="Clé redis sans TTL purgée:\n${KEYS}"
 USERNAME="PurgeRedisKeyWithoutTTL"
 EMOJI=":information_source:"
 PAYLOAD=$(jq -n --arg channel "$CHANNEL" --arg username "$USERNAME" --arg text "$SLACK_MSG" --arg icon_emoji "$EMOJI" \
